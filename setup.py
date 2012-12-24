082cb89983331cd62c6c9ee637771046dfd25bf4
Jake Johnson <jake@archive.org>
2012-12-23 21:52:20 -0800

    Cleanup for pypi compatibility.

--- setup.py
+++ setup.py
@@ -1,15 +1,34 @@
 #!/usr/bin/env python
+import sys
+import os
+
+import twiddle
+
 try:
     from setuptools import setup
 except ImportError:
     from distutils.core import setup
 
+if sys.argv[-1] == 'publish':
+    os.system('python setup.py sdist upload')
+    sys.exit()
+
+requires = [
+    'PyYAML>=3.10',
+    'pymongo>=2.4.1',
+    'tweetstream>=1.1.1',
+]
+
 setup(
     name='twiddle',
-    version='0.0.1',
+    version=twiddle.__version__,
     description='archive all the twiddles',
+    packages=['twiddle'],
+    package_data={'': ['LICENSE']},
+    include_package_data=True,
+    install_requires=requires,
     author='Jake Johnson',
     author_email='oyouareatubeo@gmail.com',
     url='http://github.com/oyouareatubeo/twiddle',
-    scripts=['twiddle'],
+    scripts=['twiddle/twiddle'],
 )
---
+++ twiddle/__init__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+"""
+twiddle
+~~~~~~~
+
+:copyright: (c) 2012 by Jake Johnson.
+:license: Apache 2.0, see LICENSE for more details.
+
+"""
+
+__title__ = 'twiddle'
+__version__ = '0.0.4'
+__author__ = 'Jake Johnson'
+__license__ = 'Apache 2.0'
+__copyright__ = 'Copyright 2012 Jake Johnson'
